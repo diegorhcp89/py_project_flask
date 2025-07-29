e3cb6ac7f117b7657c1414e403e2e9a1f6aa5477
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-07-29 12:03:40 -0300

    Criando Página de Adicionar Tarefas

--- todo_flask/app.py
+++ todo_flask/app.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 
 # Iniciando o Flask
 app = Flask(__name__)
+
+# Configuração do banco de dados
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SECRET_KEY"] = "minha_chave_secreta"
+
+db = SQLAlchemy(app)
+
+# Modelo do banco de dados
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    status = db.Column(db.Boolean, default=False)
+
+# Criar as tabelas no banco
+with app.app_context():
+    db.create_all()
 
 # Rota Principal
 @app.route("/")
