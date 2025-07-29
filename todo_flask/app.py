530b6bb333963c5326aee64010270d7c6d2f3bfd
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-07-29 12:14:32 -0300

    Criando Página de Adicioanr Tarefas

--- todo_flask/app.py
+++ todo_flask/app.py
@@ -26,5 +26,9 @@ with app.app_context():
 def index():
     return render_template("index.html")
 
+@app.route("/add", methods=["GET", "POST"])
+def add_task():
+    return render_template("add_task.html")
+
 if __name__=="__main__":
     app.run(debug=True)
